de25ea31fe4d695fbd91d22d01ff8d5e63927bd9
Donjae Wong <74441768+Donjae-Wong@users.noreply.github.com>
2022-05-12 13:34:46 +0800

    Update html2df.py

--- html2df.py
+++ html2df.py
@@ -6,4 +6,4 @@ def save_html_df(file_path, out_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         t_list = etree.HTML(f.read()).xpath(r'//*[@id="write"]/figure/table')
         text_list = [etree.tostring(t).decode('utf-8') for t in t_list]
-        [pd.read_html(t).to_csv(out_path) for t in text_list]
+        [pd.read_html(t)[0].to_csv(out_path) for t in text_list]
