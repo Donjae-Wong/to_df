3ce5ea192389646f34575f2fbd38acc8c5e8ca50
Donjae Wong <74441768+Donjae-Wong@users.noreply.github.com>
2022-05-12 13:26:53 +0800

    Create html2df.py

---
+++ html2df.py
@@ -0,0 +1,9 @@
+from lxml import etree
+import pandas as pd
+
+
+def save_html_df(file_path, out_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        t_list = etree.HTML(f.read()).xpath(r'//*[@id="write"]/figure/table')
+        text_list = [etree.tostring(t).decode('utf-8') for t in t_list]
+        [pd.read_html(t).to_csv(out_path) for t in text_list]
